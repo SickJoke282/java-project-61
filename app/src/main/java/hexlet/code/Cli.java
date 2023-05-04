0fee71c07504a5c9854d32a2010eafa1c763ffe2
Yegor <egorkrylatov18@gmail.com>
2023-05-04 12:37:42 +0500

    Add close method for Scanner object

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 
 public class Cli {
     public static void greeting() {
+        Scanner inputScan = new Scanner(System.in);
+
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
-        Scanner inputScan = new Scanner(System.in);
         String userName = inputScan.nextLine();
         System.out.println(String.format("Hello, %s!", userName));
+
+        inputScan.close();
     }
 }
