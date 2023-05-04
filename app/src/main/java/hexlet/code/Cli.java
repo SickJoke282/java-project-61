0a423de37aa226399565ca11933aee2a7c6d01a5
Yegor <egorkrylatov18@gmail.com>
2023-05-04 12:31:34 +0500

    adding new file with the logic for greeting

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,2 +1,13 @@
-package hexlet.code;public class Cli {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greeting() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        Scanner inputScan = new Scanner(System.in);
+        String userName = inputScan.nextLine();
+        System.out.println(String.format("Hello, %s!", userName));
+    }
 }
