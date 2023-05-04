8289c8ad5d17f302278be2d57b567a76bf109847
Yegor <egorkrylatov18@gmail.com>
2023-05-04 13:13:55 +0500

    add a list of games with oppurtunity to choose them

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
+import java.util.Scanner;
 class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        Scanner inputScan = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n0 - Exit");
+        System.out.print(String.format("Your choice: "));
+        int gameNumber = inputScan.nextInt();
+        switch (gameNumber) {
+            case 1:
+                Cli.greeting();
+                break;
+            case 0:
+                break;
+        }
     }
 }
