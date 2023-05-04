5068910425f10fbe5719b8dee6b14fa9f1a0c1b1
Yegor <egorkrylatov18@gmail.com>
2023-05-04 16:08:39 +0500

    Some updating in the codestyle

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -19,18 +19,18 @@ public class Even {
             } else {
                 if (userAnswer.equals("yes")) {
                     System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
-                    break;
                 } else if (userAnswer.equals("no")) {
                     System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
-                    break;
                 } else {
                     System.out.println("Your input was incorrect. Try to type 'yes' or 'no'.");
-                    break;
                 }
+                break;
             }
         }
         if (i == 3) {
             System.out.println("Congratulations, " + userName + "!");
+        } else {
+            System.out.println("Let's try again, " + userName + "!");
         }
     }
 }
