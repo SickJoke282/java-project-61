d09a01ceae24f162cdc10fc735e5a0e55d703629
Yegor <egorkrylatov18@gmail.com>
2023-05-04 15:48:47 +0500

    Even game logic

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -1,2 +1,38 @@
-package hexlet.code;public class Even {
+package hexlet.code;
+
+import org.apache.commons.lang3.RandomUtils;
+
+public class Even {
+    public static void evenNumbers() {
+        String userName = Cli.greeting();
+
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        int i = 0;
+        while (i < 3) {
+            int randomNum = RandomUtils.nextInt(1, 100);
+            System.out.println(String.format("Question: %s", randomNum));
+            System.out.print("Your answer: ");
+            String userAnswer = InputScanner.returnLine();
+
+            if (randomNum % 2 == 0 && userAnswer.equals("yes") || randomNum % 2 != 0 && userAnswer.equals("no")) {
+                System.out.println("Correct!");
+                i += 1;
+            } else {
+                if (userAnswer.equals("yes")) {
+                    System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+                    break;
+                } else if (userAnswer.equals("no")) {
+                    System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
+                    break;
+                } else {
+                    System.out.println("Your input was incorrect. Try to type 'yes' or 'no'.");
+                    break;
+                }
+            }
+        }
+        if (i == 3) {
+            System.out.println("Congratulations, " + userName + "!");
+        }
+    }
 }
