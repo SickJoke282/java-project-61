0b6f7eac35856104a932ec10e6292d7888ffd553
Yegor <egorkrylatov18@gmail.com>
2023-05-04 18:13:43 +0500

    create a method for printing about incorrect answers

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -10,6 +10,7 @@ public class Even {
         while (correctAnswers < 3) {
             int randomNum = RandomUtils.nextInt(1, 100);
             System.out.printf("Question: %d", randomNum);
+            System.out.println();
             System.out.print("Your answer: ");
             String userAnswer = InputScanner.returnLine();
 
@@ -17,13 +18,7 @@ public class Even {
                 System.out.println("Correct!");
                 correctAnswers += 1;
             } else {
-                if (userAnswer.equals("yes")) {
-                    System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
-                } else if (userAnswer.equals("no")) {
-                    System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
-                } else {
-                    System.out.println("Your input was incorrect. Try to type 'yes' or 'no'.");
-                }
+                writeAboutIncorrectAnswers(userAnswer);
                 break;
             }
         }
@@ -31,6 +26,16 @@ public class Even {
             System.out.println("Congratulations, " + userName + "!");
         } else {
             System.out.println("Let's try again, " + userName + "!");
+        }
+    }
+
+    public static void writeAboutIncorrectAnswers(String userAnswer) {
+        if (userAnswer.equals("yes")) {
+            System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+        } else if (userAnswer.equals("no")) {
+            System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
+        } else {
+            System.out.println("Your input was incorrect. Try to type 'yes' or 'no'.");
         }
     }
 }
