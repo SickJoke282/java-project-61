55895a1f736d3a92154a2ac8b1b8eda8e549a3eb
Yegor <egorkrylatov18@gmail.com>
2023-05-15 11:01:41 +0500

    finished the logic of a game

--- app/src/main/java/hexlet/code/games/Calculator.java
+++ app/src/main/java/hexlet/code/games/Calculator.java
@@ -1,2 +1,52 @@
-package hexlet.code.games;public class Calculator {
+package hexlet.code.games;
+
+import hexlet.code.Cli;
+import hexlet.code.Engine;
+import hexlet.code.InputScanner;
+import org.apache.commons.lang3.RandomUtils;
+
+public class Calculator {
+    public static void calcNumbers() {
+        String userName = Cli.greeting();
+        System.out.println("What is the result of the expression?");
+        int correctAnswers = 0;
+        String Operator = "";
+        while (correctAnswers < 3) {
+            int answer = 0;
+            int randomNum1 = RandomUtils.nextInt(1, 100);
+            int randomNum2 = RandomUtils.nextInt(1, 100);
+            int indexOfOperator = RandomUtils.nextInt(1, 4);
+            switch (indexOfOperator) {
+                case 1 -> {
+                    Operator = "+";
+                    answer = randomNum1 + randomNum2;
+                }
+                case 2 -> {
+                    Operator = "-";
+                    answer = randomNum1 - randomNum2;
+                }
+                case 3 -> {
+                    Operator = "*";
+                    answer = randomNum1 * randomNum2;
+                }
+            }
+            System.out.printf("Question: %d %s %d", randomNum1, Operator, randomNum2);
+            System.out.println();
+            System.out.print("Your answer: ");
+            String userAnswer = InputScanner.returnLine();
+
+            if (answer == Integer.parseInt(userAnswer)) {
+                System.out.println("Correct!");
+                correctAnswers += 1;
+            } else {
+                writeAboutIncorrectAnswers(userAnswer, answer);
+                break;
+            }
+        }
+        Engine.writeCongratsOrTryAgain(correctAnswers, userName);
+    }
+    public static void writeAboutIncorrectAnswers(String userAnswer, int answer) {
+        System.out.printf("'%s' is wrong answer ;(. Correct answer was '%d'.", userAnswer, answer);
+        System.out.println();
+    }
 }
