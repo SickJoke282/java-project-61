66eafb088652d79154908e49108383dd8e5dd768
Yegor <egorkrylatov18@gmail.com>
2023-05-15 10:58:23 +0500

    add a new game in the list

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,18 @@
 package hexlet.code;
 
+import hexlet.code.games.Calculator;
+import hexlet.code.games.Even;
+
 class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("2 - Even\n1 - Greet\n0 - Exit");
+        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n0 - Exit");
         System.out.print("Your choice: ");
         String gameNumber = InputScanner.returnLine();
         switch (gameNumber) {
+            case "3":
+                Calculator.calcNumbers();
+                break;
             case "2":
                 Even.guessEvenOrOddNumbers();
                 break;
