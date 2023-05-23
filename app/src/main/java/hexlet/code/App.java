ad62eb758a02779a4bfc57ace53767a3142e8694
Yegor <egorkrylatov18@gmail.com>
2023-05-23 08:35:10 +0500

    add a GCD game to the list

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,15 +1,17 @@
 package hexlet.code;
 
-import hexlet.code.games.Calculator;
-import hexlet.code.games.Even;
+import hexlet.code.games.*;
 
 class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n0 - Exit");
+        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n0 - Exit");
         System.out.print("Your choice: ");
         String gameNumber = InputScanner.returnLine();
         switch (gameNumber) {
+            case "4":
+                GCD.findGCD();
+                break;
             case "3":
                 Calculator.calcNumbers();
                 break;
