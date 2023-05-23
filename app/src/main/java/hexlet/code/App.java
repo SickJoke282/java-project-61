6d842f2f77f8fde67f4c42d8c643d7d8c5e318a7
Yegor <egorkrylatov18@gmail.com>
2023-05-23 09:30:01 +0500

    add a Progression game to the list

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,10 +5,13 @@ import hexlet.code.games.*;
 class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n0 - Exit");
+        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n0 - Exit");
         System.out.print("Your choice: ");
         String gameNumber = InputScanner.returnLine();
         switch (gameNumber) {
+            case "5":
+                Progression.guessNumberInProgression();
+                break;
             case "4":
                 GCD.findGCD();
                 break;
