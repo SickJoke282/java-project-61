e0cb25072a445050379e7a568a1e1b97ae9c75f2
Yegor <egorkrylatov18@gmail.com>
2023-05-23 08:34:33 +0500

    create a new game about the greatest common divisor

---
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -0,0 +1,54 @@
+package hexlet.code.games;
+
+import hexlet.code.Cli;
+import hexlet.code.Engine;
+import hexlet.code.InputScanner;
+import org.apache.commons.lang3.RandomUtils;
+
+public class GCD {
+    public static void findGCD() {
+        String userName = Cli.greeting();
+        System.out.println("Find the greatest common divisor of given numbers.");
+        int correctAnswers = 0;
+        while (correctAnswers < 3) {
+            int randomNum1 = RandomUtils.nextInt(1, 100);
+            int randomNum2 = RandomUtils.nextInt(1, 100);
+            int answer = calculateGCD(randomNum1, randomNum2);
+            System.out.printf("Question: %d %d", randomNum1, randomNum2);
+            System.out.println();
+            System.out.print("Your answer: ");
+            String userAnswer = InputScanner.returnLine();
+
+            if (answer == Integer.parseInt(userAnswer)) {
+                System.out.println("Correct!");
+                correctAnswers += 1;
+            } else {
+                writeAboutIncorrectAnswers(userAnswer, answer);
+                break;
+            }
+        }
+        Engine.writeCongratsOrTryAgain(correctAnswers, userName);
+    }
+    public static int calculateGCD(int num1, int num2) {
+        if (num1 > num2) {
+            while (num1 % num2 != 0) {
+                num1 = num1 % num2;
+                return calculateGCD(num2, num1);
+            }
+            return num2;
+        } else if (num2 > num1) {
+            while (num2 % num1 != 0) {
+                num2 = num2 % num1;
+                return calculateGCD(num1, num2);
+            }
+            return num1;
+        } else {
+            return num1;
+        }
+
+    }
+    public static void writeAboutIncorrectAnswers(String userAnswer, int answer) {
+        System.out.printf("'%s' is wrong answer ;(. Correct answer was '%d'.", userAnswer, answer);
+        System.out.println();
+    }
+}
