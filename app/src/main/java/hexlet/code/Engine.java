7976cb6a0033bccb589e5c906d04a8107f00ae0e
Yegor <egorkrylatov18@gmail.com>
2023-05-23 09:30:53 +0500

    add a new method which writes about incorrect answers

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,6 +1,10 @@
 package hexlet.code;
 
 public class Engine {
+    public static void writeAboutIncorrectAnswers(String userAnswer, int answer) {
+        System.out.printf("'%s' is wrong answer ;(. Correct answer was '%d'.", userAnswer, answer);
+        System.out.println();
+    }
     public static void writeCongratsOrTryAgain(int correctAnswers, String userName) {
         if (correctAnswers == 3) {
             System.out.println("Congratulations, " + userName + "!");
