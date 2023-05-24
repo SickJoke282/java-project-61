01e07970c913b5fa7bcfae5eab531245e285eb38
Yegor <egorkrylatov18@gmail.com>
2023-05-24 09:06:56 +0500

    create an overriding of a method

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,6 +5,15 @@ public class Engine {
         System.out.printf("'%s' is wrong answer ;(. Correct answer was '%d'.", userAnswer, answer);
         System.out.println();
     }
+    public static void writeAboutIncorrectAnswers(String userAnswer) {
+        if (userAnswer.equals("yes")) {
+            System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+        } else if (userAnswer.equals("no")) {
+            System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
+        } else {
+            System.out.println("Your input was incorrect. Try to type 'yes' or 'no'.");
+        }
+    }
     public static void writeCongratsOrTryAgain(int correctAnswers, String userName) {
         if (correctAnswers == 3) {
             System.out.println("Congratulations, " + userName + "!");
