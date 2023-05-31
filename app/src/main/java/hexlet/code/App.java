96e0641a84f9071f7f0a0fa10b916ccb621db6c1
SickJoke282 <menistr2@DESKTOP-VVJN9FC.localdomain>
2023-05-31 16:10:39 +0500

    some changes

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,14 +1,21 @@
 package hexlet.code;
 
-import hexlet.code.games.*;
+import hexlet.code.games.Even;
+import hexlet.code.games.Calculator;
+import hexlet.code.games.GCD;
+import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
 
 class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n0 - Exit");
+        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
         System.out.print("Your choice: ");
         String gameNumber = InputScanner.returnLine();
         switch (gameNumber) {
+            case "6":
+                Prime.guessPrimeNumber();
+                break;
             case "5":
                 Progression.guessNumberInProgression();
                 break;
