325a4c741d9bd7e4502c37f10425b11146551d96
SickJoke282 <menistr2@DESKTOP-VVJN9FC.localdomain>
2023-06-01 00:02:13 +0500

    add some methods for better readability, create an array with operators

--- app/src/main/java/hexlet/code/games/Calculator.java
+++ app/src/main/java/hexlet/code/games/Calculator.java
@@ -6,32 +6,18 @@ import hexlet.code.InputScanner;
 import org.apache.commons.lang3.RandomUtils;
 
 public class Calculator {
-    static final int MAX_INDEX = 4;
+    static final int MAX_INDEX = 3;
     public static void calcNumbers() {
         String userName = Cli.greeting();
         System.out.println("What is the result of the expression?");
         int correctAnswers = 0;
-        String operator = "";
+        String[] operations = {"+", "-", "*"};
         while (correctAnswers < Engine.TOTAL_CORRECT_ANSWERS) {
-            int answer = 0;
             int randomNum1 = RandomUtils.nextInt(1, Engine.MAX_RANDOM_NUM);
             int randomNum2 = RandomUtils.nextInt(1, Engine.MAX_RANDOM_NUM);
-            int indexOfOperator = RandomUtils.nextInt(1, MAX_INDEX);
-            switch (indexOfOperator) {
-                case 1 -> {
-                    operator = "+";
-                    answer = randomNum1 + randomNum2;
-                }
-                case 2 -> {
-                    operator = "-";
-                    answer = randomNum1 - randomNum2;
-                }
-                case MAX_INDEX - 1 -> {
-                    operator = "*";
-                    answer = randomNum1 * randomNum2;
-                }
-                default -> { }
-            }
+            int indexOfOperator = RandomUtils.nextInt(0, MAX_INDEX);
+            String operator = defineAnOperator(operations, indexOfOperator);
+            int answer = calculate(operator, randomNum1, randomNum2);
             System.out.printf("Question: %d %s %d", randomNum1, operator, randomNum2);
             System.out.println();
             System.out.print("Your answer: ");
@@ -46,5 +32,36 @@ public class Calculator {
             }
         }
         Engine.writeCongratsOrTryAgain(correctAnswers, userName);
+    }
+    static int calculate(String operator, int randomNum1, int randomNum2) {
+        switch (operator) {
+            case "+" -> {
+                return randomNum1 + randomNum2;
+            }
+            case "-" -> {
+                return randomNum1 - randomNum2;
+            }
+            case "*" -> {
+                return randomNum1 * randomNum2;
+            }
+            default -> {
+                return 0;
+            }
+        }
+    }
+    static String defineAnOperator(String[] operations, int indexOfOperator) {
+        String operator = "";
+        switch (indexOfOperator) {
+            case 0:
+                operator = operations[0];
+                break;
+            case 1:
+                operator = operations[1];
+                break;
+            case MAX_INDEX - 1:
+                operator = operations[MAX_INDEX - 1];
+            default: { }
+        }
+        return operator;
     }
 }
